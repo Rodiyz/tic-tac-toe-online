c17d1e1498f6b484dcadec8d1232140bc21c977f
Rodiyz <malinkin54ru@gmail.com>
2025-06-29 08:55:44 +0700

    button

--- src/features/games-list/containers/create-button.tsx
+++ src/features/games-list/containers/create-button.tsx
@@ -3,25 +3,27 @@
 import { Button } from "@/shared/ui/button";
 import { createGameAction } from "../actions/create-game";
 import { useActionState } from "@/shared/lib/react";
-import { matchEither, right } from "@/shared/lib/either";
+import { mapLeft, right } from "@/shared/lib/either";
 
 export function CreateButton() {
 
-  const [data, dispatch, isPending] =useActionState(
+  const [state, dispatch, isPending] =useActionState(
     createGameAction,
     right(undefined),
   );
 
   return (
-    <div className="flex flex-col gap-1">
-      <Button disabled={isPending} onClick={dispatch}>Создать игру</Button>
-      {matchEither(data, {
-        right: () => null,
-        left: (e) => ({
-          ["can-create-only-one-game"]: "Вы можете создать только одну игру",
-          ["user-not-found"]: "Пользователь не найден",
-        })[e],
-        })}
-    </div>
+      <Button 
+        disabled={isPending} 
+        onClick={dispatch}
+        error={mapLeft(
+          state,
+          (e) =>
+            ({
+              ["can-create-only-one-game"]: "Вы можете создать только одну игру",
+              ["user-not-found"]: "Пользователя нет",
+            })[e],
+        )}>Создать игру</Button>
+      
   );
 }
