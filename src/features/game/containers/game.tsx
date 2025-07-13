b5eb6cd46433abbe299db68f8ae6c589d2f03069
Rodiyz <malinkin54ru@gmail.com>
2025-07-13 17:21:36 +0700

    game

--- src/features/game/containers/game.tsx
+++ src/features/game/containers/game.tsx
@@ -1,3 +1,25 @@
-export function Game() {
-  return <div>Game</div>;
+import { GameId } from "@/kernel/ids";
+import { GameClient } from "./game-client";
+import { getGameById, startGame } from "@/entities/game/server";
+import { getCurrentUser } from "@/entities/user/server";
+import { redirect } from "next/navigation";
+
+export async function Game({ gameId }: { gameId: GameId }) {
+  const user = await getCurrentUser();
+
+  let game = await getGameById(gameId);
+
+  if (!game || !user) {
+    redirect("/");
+  }
+
+  if (user) {
+    const startGameResult = await startGame(gameId, user);
+
+    if (startGameResult.type === "right") {
+      game = startGameResult.value;
+    }
+  }
+
+  return <GameClient defaultGame={game} player={user} />;
 }
